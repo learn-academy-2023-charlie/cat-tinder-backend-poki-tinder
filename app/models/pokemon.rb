b97b212c6600d06c144c064c0b821eb94b28219b
henrike32 <henrique.lambertucci@gmail.com>
2023-06-12 10:14:52 -0400

    app/models/pokemon.rb changed the validation to size be always greater than 0 and only accept floats

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,5 @@
 class Pokemon < ApplicationRecord
     validates :name, :size, :species, :pokemon_type, :sound, :image, presence: true
     validates :image, length: {minimum: 10}
+    validates :size, numericality: { greater_than_or_equal_to: 0, only_only_float: true }
 end
